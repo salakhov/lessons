6d3c09eb3e3fc54582e9fc5ecaac94da5ad8f9ca
salakhov <salakhov@inbox.ru>
2019-05-02 14:59:59 +0300

    Выполнено 1 задание

--- src/part1/lesson04/task01/Main.java
+++ src/part1/lesson04/task01/Main.java
@@ -1,6 +1,7 @@
 package part1.lesson04.task01;
 
-import java.util.concurrent.ForkJoinPool;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Задание 1. Написать класс MathBox, реализующий следующий функционал:
@@ -22,17 +23,39 @@ import java.util.concurrent.ForkJoinPool;
 
 public class Main {
     public static void main(String[] args) {
+        //2 различных массива Numbers
         Number [] numbers = new Number[10];
         for (int i=0;i<numbers.length;i++){
             numbers[i] = i;
         }
-
+        Number [] numbers2 = new Number[11];
+        for (int i=0;i<numbers2.length;i++){
+            numbers2[i] = i;
+        }
+         //3 объекта. 2 одинаковых для проверка функций equals,hashset
         MathBox mathBox = new MathBox(numbers);
-        mathBox.printCollection();
-        System.out.println("Summa: "+mathBox.summmator());
-        mathBox.splitter(2);
-        mathBox.printCollection();
+        MathBox mathBox2 = new MathBox(numbers2);
+        MathBox mathBox3= new MathBox(numbers);
 
+        //В карту добавляем 3 объекта. Так как 2 одинаковых - сохраняются 2
+        Map<Integer,MathBox> map = new HashMap<Integer,MathBox>();
+        map.put(mathBox.hashCode(),mathBox);
+        map.put(mathBox2.hashCode(),mathBox2);
+        map.put(mathBox2.hashCode(),mathBox3);
+
+        System.out.println("Размер карты:"+map.size());
+
+        //Цикл по карте и вызов функций для каждого MathBox
+        for(Map.Entry<Integer, MathBox> item : map.entrySet()){
+            System.out.println("\n**** Начальное значение MathBox");
+            System.out.println("key: "+item.getKey()+" value:"+item.getValue().toString());
+            System.out.println("Summa: "+item.getValue().summmator());
+            item.getValue().splitter(2.0);
+            System.out.println("Значение MathBox после деления на 2");
+            System.out.println("key: "+item.getKey()+" value:"+item.getValue().toString());
+            System.out.println("Значение MathBox после удаления 0");
+            item.getValue().safeDelete(0.0);
+            System.out.println("key: "+item.getKey()+" value:"+item.getValue().toString());
+        }
     }
-
 }
--- src/part1/lesson04/task01/MathBox.java
+++ src/part1/lesson04/task01/MathBox.java
@@ -1,72 +1,112 @@
 package part1.lesson04.task01;
-
-//import java.util.ArrayList;
-import javax.print.attribute.standard.NumberUp;
 import java.util.*;
 
 public class MathBox {
-    private HashSet<Number> collection = new HashSet();
+    private Set<Number> collection = new HashSet<>();
 
     /**
      *  * Конструктор на вход получает массив Number. Элементы не могут повторяться. Элементы массива внутри объекта
      *  * раскладываются в подходящую коллекцию (выбрать самостоятельно).
-     * @param number
+     * @param number - массив Number
      */
     public MathBox(Number [] number) {
-        for (Number num:number)
+        for (Number num:number) {
             collection.add(num);
-    }
-
-    public void printCollection(){
-        Iterator it = collection.iterator();
-        while(it.hasNext())
-            System.out.println(it.next());
-
+        }
     }
 
     /**
      * метод summator, возвращающий сумму всех элементов коллекции.
      */
     public int summmator(){
-        Iterator<Number> it = collection.iterator();
+        Iterator it = collection.iterator();
         Integer sum=0;
         while(it.hasNext())
-            sum=it.next().intValue()/sum;
+            sum=(Integer) it.next()+sum;
         return sum;
     }
 
     /**
-     * выполняющий поочередное деление всех хранящихся в объекте элементов на делитель, являющийся аргументом метода.
+     * Деление всех хранящихся в объекте элементов на делитель, являющийся аргументом метода.
      * Хранящиеся в объекте данные полностью заменяются результатами деления.
      * @param num
      */
-    public void splitter(int num){
-        Iterator<Double> iterator=collection.iterator();
+    public void splitter(Double num){
+        Set collectionCopy = new HashSet<Double>();
+        Iterator<Number> iterator=collection.iterator();
         while(iterator.hasNext()) {
-            Double d = iterator.next() + num;
-            collection.add(d);
+            Double d = iterator.next().doubleValue() / num;
+            collectionCopy.add(d);
+            iterator.remove();
         }
+        collection=collectionCopy;
     }
 
     /**
-     * Создать метод, который получает на вход Integer и если такое значение есть в коллекции, удаляет его.
+     * Метод, который получает на вход Number и если такое значение есть в коллекции, удаляет его.
      */
-    public void delete(int num){
-
+    public void safeDelete(Number num){
+        if(collection.contains(num)){
+            collection.remove(num);
+        }
     }
 
+    public Set<Number> getCollection() {
+        return collection;
+    }
+
+    /**
+     * переопределени hashcode. Вычисляется на основании хешкода строки, полученной из всех элементов коллекции
+     * @return
+     */
     @Override
     public int hashCode() {
-        return super.hashCode();
+        //return collection.hashCode();
+        String str="";
+        for (Number num:collection) {
+            str = num.toString() + str;
+        }
+        return str.hashCode();
     }
 
+    /**
+     * Переопределение функции равентсва двух объектов. Два объекта равны если равны содержимое коллекций
+     * @param obj
+     * @return
+     */
     @Override
     public boolean equals(Object obj) {
-        return super.equals(obj);
+        MathBox mathBox2 = (MathBox)obj;
+        Set<Number> collection2 = mathBox2.getCollection();
+        Iterator<Number> iterator1=collection.iterator();
+        Iterator<Number> iterator2=mathBox2.getCollection().iterator();
+        boolean result = true;
+        if(collection.size()==collection2.size()){
+            while(iterator1.hasNext()){
+                Number first =iterator1.next();
+                Number second =iterator2.next();
+                if(!first.equals(second)){
+                    result =false;
+                    break;
+                }
+            }
+        }
+        else
+            result=false;
+
+        return result;
     }
 
+    /**
+     * Переопределение метода toString. печать всех элементов коллекции через точку с запятой
+     * @return
+     */
     @Override
     public String toString() {
-        return super.toString();
+        Iterator it = collection.iterator();
+        String result="";
+        while(it.hasNext())
+            result = it.next().toString()+";"+result;
+        return result;
     }
 }
