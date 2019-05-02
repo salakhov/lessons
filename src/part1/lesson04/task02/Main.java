243dd4ad822fa83e5609482e445bee40d8109631
salakhov <salakhov@inbox.ru>
2019-05-02 15:38:12 +0300

    Выполнено 2 задание

--- src/part1/lesson04/task02/Main.java
+++ src/part1/lesson04/task02/Main.java
@@ -10,7 +10,21 @@ package part1.lesson04.task02;
 
 public class Main {
     public static void main(String[] args) {
+        Object [] objects=new Object[3];
+        objects[0] = "String";
+        Integer value = new Integer(2);
+        objects[1] = value;
+        objects[2] = new Double(4.5);
 
+        ObjectBox objBox = new ObjectBox();
 
+        for(int i=0;i<objects.length;i++){
+            objBox.addObject(objects[i]);
+        }
+        System.out.println("Печать элементов объекта");
+        objBox.dump();
+        System.out.println("Печать элементов объекта после удаление 2");
+        objBox.deleteObject(objects[2]);
+        objBox.dump();
     }
 }
