b6d611316f64a754e0f9c082ab1a7d5d8f438cc5
salakhov <salakhov@inbox.ru>
2019-05-05 13:30:53 +0300

    Выполнено задание урока 4

--- src/part1/lesson05/task01/Main.java
+++ src/part1/lesson05/task01/Main.java
@@ -13,7 +13,51 @@ package part1.lesson05.task01;
  */
 
 public class Main {
-    public static void main(String[] args) {
 
+
+    public static void main(String[] args) {
+        Master master1=new Master("Хelen",10,"MAN");
+        Master master2 =new Master("Huan",30,"MAN");
+        Master master3 =new Master("Alina",20,"WOMAN");
+        Master master4 =new Master("Alina",30,"WOMAN");
+
+        Pet pet1=new Pet(1,"barsik",master1,10);
+        Pet pet2=new Pet(2,"mursic",master3,20);
+        Pet pet3=new Pet(3,"marusya",master2,30);
+        Pet pet4=new Pet(4,"vsevolod nikolaevich",master2,20);
+        Pet pet5=new Pet(5,"kizka",master4,24);
+
+        System.out.println("*** Добавление животного в общий список");
+        PetIndex index = new PetIndex();
+        try {
+            index.addPet(pet1);
+            index.addPet(pet2);
+            index.addPet(pet3);
+            index.addPet(pet4);
+            index.addPet(pet5);
+        }
+        catch (PetInCollectionException e){
+            e.printStackTrace();
+        }
+
+        index.printUnsorted();
+        System.out.println("-----");
+        System.out.println("*** Поиск животного по кличке");
+        Pet findPet = index.findPet("marusya");
+        if(findPet!=null) {
+            System.out.println("Кот " + findPet.getName() + " найден "+ findPet.toString());
+        }
+        System.out.println("-----");
+        System.out.println("Изменение хозяина у животного с идентификатором 3");
+        index.modifyPetMasterById(3,master1);
+        index.printUnsorted();
+        System.out.println("-----");
+        System.out.println("Изменение имени у животного с идентификатором 3");
+        index.modifyPetNameById(3,"hrusha");
+        index.printUnsorted();
+        System.out.println("-----");
+        System.out.println("Вывод на экран животных в отсортированном порядке");
+        index.printSorted();
+        System.out.println("-----");
     }
 }
--- src/part1/lesson05/task01/Pet.java
+++ src/part1/lesson05/task01/Pet.java
@@ -1,7 +1,78 @@
 package part1.lesson05.task01;
 
-public class Pet {
+/**
+ * Класс домашнее животное. Наследуется от класса Comparable для обеспечение записи в TreeSet для сортировки
+ * Пoля id - идентификатор
+ *      name - Кличка
+ *      master - хозяин
+ *      weight - вес.
+ *
+ */
+
+public class Pet implements Comparable {
     private int id;
     private String name;
-    private Person master;
+    private Master master;
+    private int weight;
+
+    public Pet(int id, String name, Master master,int weight) {
+        this.id = id;
+        this.name = name;
+        this.master = master;
+        this.weight = weight;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public Master getMaster() {
+        return master;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setMaster(Master master) {
+        this.master = master;
+    }
+    public int getWeight() {
+        return weight;
+    }
+
+    @Override
+    public String toString() {
+        return "Pet{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", master=" + master +
+                ", weight=" + weight +
+                '}';
+    }
+
+    /**
+     * Сначала сортируются по Хозяину (метод сравнения хохяев переопределен в классе Master)
+     * Затем сортируем по алфавиту имени и весу.*
+     * @param o
+     * @return
+     */
+    @Override
+    public int compareTo(Object o) {
+        Pet pet = (Pet)o;
+        int result = 0;
+        int resultMaster = this.getMaster().compareTo(pet.getMaster());
+        int resultWeight = this.getWeight() - pet.getWeight(); //0 - равный, <0 this меньше,
+        int resultName = this.getName().compareTo(pet.getName()); //0 - равный, <0 this меньше,
+
+        if(resultMaster==0) {
+            if (resultName == 0) {
+                result = resultWeight;
+            }
+            else
+                result = resultName;
+        }
+        else{
+            result=resultMaster;
+        }
+        return result;
+    }
 }
+
