62d85d95ef10f68525c97d54bef92d823702b1da
salakhov <salakhov@inbox.ru>
2019-05-14 19:58:09 +0300

    убран комментарий

--- src/part1/lesson06/task02/FileGenerator.java
+++ src/part1/lesson06/task02/FileGenerator.java
@@ -25,16 +25,7 @@ public class FileGenerator {
             catch (Exception ex){
                 ex.printStackTrace();
             }
-//            try(ByteArrayOutputStream bos = new ByteArrayOutputStream(new FileOutputStream(filename))){
-//                Text text = new Text(size);
-//                text.genText(words,probability);
-//                byte[] buff = text.toString().getBytes();
-//                bos.write(buff,0,size);
-//                text.printText();
-//            }
-//            catch (Exception ex){
-//                ex.printStackTrace();
-//            }
+
 
         }
     }
