4686177854faa7431ab00d35b82520cba2cdaf62
Михаил Салахов <salahov@primetver.ru>
2019-05-23 17:37:25 +0300

    7е задание. Добавлн Кэш

--- src/part1/lesson07/taks01/Factorial.java
+++ src/part1/lesson07/taks01/Factorial.java
@@ -2,6 +2,7 @@ package part1.lesson07.taks01;
 
 import java.math.BigInteger;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Факториал 170 = 7.257415615307999e+306
@@ -11,21 +12,33 @@ import java.util.ArrayList;
 
 public class Factorial implements Runnable {
     ArrayList <Integer> array;
+    static HashMap<Integer,BigInteger> cashe = new HashMap<>();
     public Factorial(ArrayList array) {
         this.array = array;
     }
 
     @Override
     public void run() {
-        System.out.println("factorial"+ array.get(1)+" = "+this.CalcFactorial(400));
+        System.out.println("factorial"+ array.get(1)+" = "+this.CalcFactorial(10));
+        System.out.println("factorial"+ array.get(1)+" = "+this.CalcFactorial(11));
     }
 
     private BigInteger CalcFactorial(Integer number){
 
         BigInteger result = BigInteger.ONE;
+        BigInteger casheRet = cashe.get(number);
+        //проверяем если число есть в кеше возвращаем из кеша.
+        if(casheRet!=null) {
+            System.out.println("exec from cashe");
+            return casheRet;
+        }
+        //TODO проверяем есть ли в кеше число меньшее
+
+        //вычисляем факториал по честному
         for (int i=1;i<=number;i++){
             result = result.multiply(BigInteger.valueOf(i));
         }
+        cashe.put(number,result);
         return result;
     }
 
