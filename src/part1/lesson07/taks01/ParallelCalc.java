8122a93bd0d00c262092f77fd8934aad78136951
salakhov <salakhov@inbox.ru>
2019-06-01 13:29:54 +0300

    Запуск с помощью ExecutorService в паралельных потоках

--- src/part1/lesson07/taks01/ParallelCalc.java
+++ src/part1/lesson07/taks01/ParallelCalc.java
@@ -8,6 +8,7 @@ import java.util.NoSuchElementException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 
 /**
@@ -30,63 +31,68 @@ public class ParallelCalc {
      * Нечколько чисел вычисляются паралельно
      */
     public void CalcClassicMethod() {
-        Thread[] thread = new Thread[parralelism];
         Factorial[] fact = new Factorial[parralelism];
 
         Iterator it = array.iterator();
-
+        Future[] future = new Future[parralelism];
+        ExecutorService service = null;
         //Цикл по коллекции пока есть элементы
         while (it.hasNext()) {
             //Цикл по потокам, запуск потоков с указанной степенью паралелизма
-            for (int calc = 0; calc < thread.length; calc++) {
+            for (int calc = 0; calc < parralelism; calc++) {
                 try {
                     Integer value = (Integer) it.next();
                     fact[calc] = new Factorial(value);
-                    thread[calc] = new Thread(fact[calc]);
-                    ExecutorService service = Executors.newFixedThreadPool(parralelism);
-                    service.execute(fact[calc]);
-                    //thread[calc].start();
-                } catch (NoSuchElementException err) {
-                    //Если кончились элементы и на все потоки не хватило
+                    service = Executors.newFixedThreadPool(parralelism);
+                    future[calc] = service.submit(fact[calc]);
 
+                } catch (NoSuchElementException err) {
                     break;
+                } finally {
+                    service.shutdown();
                 }
             }
         }
     }
 
     /**
-     * Вычисление распаралеливанием одного числа
-     *
+     * Вычисление распаралеливанием одного числа. Черновик
      */
 
     public void CalcParallelMethod() {
-        //Iterator <Integer> it = array.iterator();
 
-        //while (it.hasNext()) {
-        for (Integer i : array) {
-            //Integer value = it.next();
-            Integer value = i;
-            Integer middleValue = Math.abs(value / 2);
-
-            ParallelFactorial calc1 = new ParallelFactorial(1, middleValue);
-            ParallelFactorial calc2 = new ParallelFactorial(middleValue+1, value);
-            try {
-                ExecutorService execute1 = Executors.newFixedThreadPool(2);
-                Future future1;
-                future1 = execute1.submit(calc1);
-                Future future2;
-                future2 = execute1.submit(calc2);
-
-                BigInteger result1 = (BigInteger) future1.get();
-                BigInteger result2 = (BigInteger) future2.get();
-
-                System.out.println(result1 + " +++ " + result2);
-
-            } catch (Exception err) {
-                err.printStackTrace();
-            }
-        }
+//        for (Integer i : array) {
+//            //Integer value = it.next();
+//            Integer value = i;
+//            Integer middleValue = Math.abs(value / 2);
+//
+//            ParallelFactorial calc1 = new ParallelFactorial(1, middleValue);
+//            ParallelFactorial calc2 = new ParallelFactorial(middleValue + 1, value);
+//            ExecutorService execute=null;
+//            Future future1;
+//            Future future2;
+//            try {
+//                 execute = Executors.newFixedThreadPool(2);
+//
+//                future1 = execute.submit(calc1);
+//
+//                future2 = execute.submit(calc2);
+//
+//                BigInteger result1 = (BigInteger) future1.get();
+//                BigInteger result2 = (BigInteger) future2.get();
+//                BigInteger result = result1.multiply(result2);
+//
+//                System.out.println("factorial " + value + " "+ result1 );
+//
+//            } catch (Exception err) {
+//                err.printStackTrace();
+//            }
+//            finally {
+//                if(execute!=null)
+//                    execute.shutdown();
+//
+//            }
+//        }
 
 
     }
