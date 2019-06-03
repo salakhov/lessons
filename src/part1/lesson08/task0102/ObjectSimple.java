7df6f69d2f9bab94e1f0d48409585bb7a5d5f8c3
Михаил Салахов <salahov@primetver.ru>
2019-06-03 14:40:52 +0300

    Добавлен ссылочный объект

---
+++ src/part1/lesson08/task0102/ObjectSimple.java
@@ -0,0 +1,11 @@
+package part1.lesson08.task0102;
+
+public class ObjectSimple {
+    private String text;
+    private Integer number;
+
+    public ObjectSimple(String text, Integer number) {
+        this.text = text;
+        this.number = number;
+    }
+}
