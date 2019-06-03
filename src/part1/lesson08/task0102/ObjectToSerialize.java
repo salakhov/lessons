96ffe28830e6a7ae8a1faf5b45a5b3222aef6278
Михаил Салахов <salahov@primetver.ru>
2019-06-03 12:16:00 +0300

    Добавлен ссылочный объект

---
+++ src/part1/lesson08/task0102/ObjectToSerialize.java
@@ -0,0 +1,25 @@
+package part1.lesson08.task0102;
+
+import java.io.Serializable;
+
+/**
+ * Ссылочный объект для загрузки через рефлексию
+ */
+
+public class ObjectToSerialize implements Serializable {
+    private String objectName;
+    private Integer objectCount;
+
+    public ObjectToSerialize(String objectName, Integer objectCount) {
+        this.objectName = objectName;
+        this.objectCount = objectCount;
+    }
+
+    @Override
+    public String toString() {
+        return "ObjectToSave{" +
+                "objectName='" + objectName + '\'' +
+                ", objectCount=" + objectCount +
+                '}';
+    }
+}
