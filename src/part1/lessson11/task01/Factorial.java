b2522614201c93e3309fa6e981d99beb75d52d62
Михаил Салахов <salahov@primetver.ru>
2019-06-05 16:35:33 +0300

    Шаблон для 11  задания

---
+++ src/part1/lessson11/task01/Factorial.java
@@ -0,0 +1,57 @@
+package part1.lessson11.task01;
+
+import java.math.BigInteger;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Класс занимается вычислением факториала числа
+ * cashe - коллекция куда складывается вычисленный факториал для ускорения дальнейших вычислений
+ * number - число
+ * <p>
+ * справочная информация
+ * Факториал 170 = 7.257415615307999e+306
+ * Integer (MAX: 2147483647, MIN -2147483647)
+ * bigInteger 1234567890123456890
+ */
+
+public class Factorial implements Runnable {
+    private static Map<Integer, BigInteger> cache = new ConcurrentHashMap<>();
+    private Integer number;
+
+    public Factorial(Integer number) {
+        this.number = number;
+    }
+
+    /**
+     * Функция интеллектуального вычисления факториала с использованием кеша
+     *
+     * @return
+     */
+    public BigInteger CalcFactorial() {
+        BigInteger result = null;
+        //Пытаемся взять из кеша
+        if (!cache.isEmpty())
+            result = getResultFromCashe();
+        //вычисляем факториал по честному
+        if (result == null) {
+            result = BigInteger.ONE;
+            for (int i = 1; i <= this.number; i++) {
+                result = result.multiply(BigInteger.valueOf(i));
+            }
+        }
+        //кладем в кеш
+        cache.put(this.number, result);
+        return result;
+    }
+
+    private BigInteger getResultFromCashe() {
+        return cache.get(this.number);
+    }
+
+    @Override
+    public void run() {
+        BigInteger temp = CalcFactorial();
+        System.out.println("Factorial " + number + "=" + temp);
+    }
+}
---
+++ src/part1/lessson11/task01/Main.java
@@ -0,0 +1,39 @@
+package part1.lessson11.task01;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Дан массив случайных чисел. Написать программу для вычисления факториалов всех элементов массива. Использовать пул потоков для решения задачи.
+ * Особенности выполнения:
+ * Для данного примера использовать рекурсию - не очень хороший вариант, т.к. происходит большое выделение памяти, очень вероятен StackOverFlow.
+ * Лучше перемножать числа в простом цикле при этом создавать объект типа BigInteger
+ * По сути, есть несколько способа решения задания:
+ * 1) распараллеливать вычисление факториала для одного числа
+ * 2) распараллеливать вычисления для разных чисел
+ * 3) комбинированный
+ * При чем вычислив факториал для одного числа, можно запомнить эти данные и использовать их для вычисления другого, что будет гораздо быстрее
+ */
+
+//DONE TODO  1. Thread Pool Executor а не запускать все потоки одновременно
+//DONE TODO  2. Разобраться с callable
+public class Main {
+    public static void main(String[] args) {
+        Random rnd = new Random();
+        List<Integer> array = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            //int num = rnd.nextInt(20);
+            int num = i;
+            array.add(num);
+        }
+
+        ParallelCalc parallelCalc = new ParallelCalc(array);
+        parallelCalc.CalcClassicMethod();
+        //parallelCalc.CalcParallelMethod();
+
+        array.forEach(System.out::println);
+    }
+
+
+}
