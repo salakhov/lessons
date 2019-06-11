c806f8752de97bf7b23d00800616f216bbe98cba
salakhov <salakhov@inbox.ru>
2019-06-11 11:35:44 +0300

    устранена ошибка в 7 уроке

--- src/part1/lesson07/task01/ParallelCalc.java
+++ src/part1/lesson07/task01/ParallelCalc.java
@@ -1,13 +1,9 @@
 package part1.lesson07.taks01;
 
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
 
 
 /**
@@ -30,26 +26,17 @@ public class ParallelCalc {
      * Нечколько чисел вычисляются паралельно
      */
     public void CalcClassicMethod() {
-        Factorial[] fact = new Factorial[parralelism];
-
         Iterator it = array.iterator();
-        Future[] future = new Future[parralelism];
         ExecutorService service = null;
         //Цикл по коллекции пока есть элементы
-        while (it.hasNext()) {
-            //Цикл по потокам, запуск потоков с указанной степенью паралелизма
-            for (int calc = 0; calc < parralelism; calc++) {
-                try {
-                    Integer value = (Integer) it.next();
-                    fact[calc] = new Factorial(value);
-                    service = Executors.newFixedThreadPool(parralelism);
-                    future[calc] = service.submit(fact[calc]);
-
-                } catch (NoSuchElementException err) {
-                    break;
-                } finally {
-                    service.shutdown();
-                }
+        while (it.hasNext()){
+            try{
+                Integer value = (Integer) it.next();
+                service = Executors.newFixedThreadPool(parralelism);
+                service.submit(new Factorial(value));
+            }
+            finally {
+                service.shutdown();
             }
         }
     }
