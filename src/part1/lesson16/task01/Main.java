c0b832597aad7b7afeb3920b2cd9c6b0804adaa7
salakhov <salakhov@inbox.ru>
2019-06-13 22:03:50 +0300

    добавлено логирование в соответствии с заданием

--- src/part1/lesson16/task01/Main.java
+++ src/part1/lesson16/task01/Main.java
@@ -1,9 +1,11 @@
 package part1.lesson16.task01;
 
 import org.apache.logging.log4j.core.Logger;
+import org.apache.logging.log4j.LogManager;
 
 import java.sql.*;
-import java.util.logging.LogManager;
+//import java.util.logging.LogManager;
+//import java.util.logging.Logger;
 
 /**
  * ДЗ_16
@@ -21,19 +23,27 @@ import java.util.logging.LogManager;
  */
 
 public class Main {
-
+    private static Logger log = (Logger) LogManager.getLogger(Main.class);
     public static void main(String[] args) {
         try {
             String url = "jdbc:postgresql://localhost:5432/inno?user=sam&password=Gfhjkm";
-
+            log.info("Open connection "+ url );
             Connection cn = DriverManager.getConnection(url);
             SqlOperations sqlTest = new SqlOperations(cn);
+            log.info("Вставка записи в БД");
             sqlTest.makeInsert();
+
+            log.info("Параметрическая вставка записи в БД");
             sqlTest.makeParamInsert();
+
+            log.info("Пакетная вставка записи в БД");
             sqlTest.makeBatchInsert();
+            log.info("Параметрическая выборка из БД");
             sqlTest.makeParamSelect();
+            log.info("Вставка и откат в точке сохранения");
             sqlTest.makeSaveTransaction();
         } catch (SQLException ex) {
+            log.error(ex.getMessage());
             ex.printStackTrace();
         }
     }
